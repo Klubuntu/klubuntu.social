69987fb48fbff86db11383eba84fa342d4668f91
Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2022-10-06 23:34:57 +0200

    Remove unused/useless code & Optimization Page
    v3.1 (2022)

--- beta/easycookie.js
+++ beta/easycookie.js
@@ -1,148 +1,49 @@
-/* EasyCookie v2.1 [Rewrited] */
+/* EasyCookie v3 [Rewrited Class Edition] */
 /* Created by Klubuntu */
 /* Source Code: https://github.com/Klubuntu/EasyCookieJS */
 
+const $prefix = '[EasyCookie] ';
 
-//easyCookieInterpreter
-function eCI(cmd, arg1 = "", arg2 = "") {
-    msg = "[EasyCookie] ";
-    function addCookie() {
-        name = arg1
-        val = arg2
-        if (!name == "" || !name == null) {
-            if (!val == "" || !val == null) {
-                let usrcookie = getCookie(get = 1);
-                if (usrcookie != "") {
-                    console.log(msg + "Cookie " + name + " has been already created")
-                } else {
-                    if (name != "" && [name] != null) {
-                        document.cookie = name + "=" + val
-                        console.log(msg + "Cookie " + name + " has been created")
-                    }
-                };
-            }
-            else {
-                console.warn(msg + "Cookie value no set")
-            }
+class EasyCookie {
+    static add(cookieName, cookieValue){
+        try{
+      	  document.cookie =  cookieName + "=" + cookieValue +  "; expires=Thu, 31 Dec 2023 23:59:00 UTC";
+          return $prefix + cookieName +  " created";
         }
-        else {
-            console.warn(msg + "Cookie name no set")
-        }
+        catch{
+          return $prefix + "invalid cookie name or value";
+	    }
     }
-    function getCookie(get = 0) {
-        name = arg1
-        if (!name == "" || !name == null) {
-            //cname = cookiename
-            cname = name;
-            let tmpName = cname + "=";
-            let decodedCookie = decodeURIComponent(document.cookie);
-            let ca = decodedCookie.split(';');
-            for (let i = 0; i < ca.length; i++) {
-                let c = ca[i];
-                while (c.charAt(0) == ' ') {
-                    c = c.substring(1);
-                }
-                if (c.indexOf(tmpName) == 0) {
-                    xcookie = c.substring(tmpName.length, c.length);
-                    return c.substring(tmpName.length, c.length);
-                }
-            }
-            if (get == 0) {
-                return msg + "Cookie " + name + " no exists";
-            }
-            else if (get == 1) {
-                return "";
-            }
-
-        }
-        else {
-            console.warn(msg + "Cookie name no set")
-        }
-    }
-    function editCookie() {
-        name = arg1
-        val = arg2
-        if (!name == "" || !name == null) {
-            if (!val == "" || !val == null) {
-                let usrcookie = getCookie(get = 1);
-                if (usrcookie != "") {
-                    document.cookie = document.cookie = name + "=" + val;
-                } else {
-                    if (name != "" && [name] != null) {
-                        console.log(msg + "Cookie ", name, " not found.")
-                    }
-                }
-            }
-            else {
-                console.warn(msg + "Cookie value no set")
-            }
-        }
-        else {
-            console.warn(msg + "Cookie name no set")
-        }
-    }
-    function delCookie() {
-        name = arg1
-        if (!name == "" || !name == null) {
-            let usrcookie = getCookie(get = 1);
-            if (usrcookie != "") {
-                document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-            } else {
-                if (name != "" && [name] != null) {
-                    console.log(msg + "Cookie ", name, " not found.")
-                }
-            }
-        }
-        else {
-            console.warn(msg + "Cookie name no set")
-        }
+    static get(cookieName){
+    	const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${cookieName}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
     }
 
-    function version(){
-        return(
-            msg + "Version 2.1 [Rewrited]" + "\n" + "Check new release on https://github.com/Klubuntu/EasyCookieJS"
-        )
+    static edit(cookieName, cookieValue){
+        return this.add(cookieName, cookieValue);
     }
 
+    static remove(cookieName){
+        try{
+            document.cookie = cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            document.cookie = cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+            return $prefix + cookieName + " deleted"
+      }
+        catch{
+            return $prefix +  "Problem with delete cookie " + cookieName
+      }
+    }
 
-    if (cmd == "add") {
-        addCookie();
-    }
-    else if (cmd == "get") {
-        getCookie();
-    }
-    else if (cmd == "edit") {
-        editCookie();
-    }
-    else if (cmd == "del") {
-        delCookie();
-    }
-    else if(cmd == "ver"){
-        console.log(version());
-    }
-    else {
-        return msg + "Unknown Command";
+    /* Setup Aliases */
+        
+    static delete(cookieName){
+        return this.remove(cookieName);
+    }  	
+    static del(cookieName){
+        return this.delete(cookieName);
+    }    
+    static find(cookieName){
+        return this.check(cookieName);
     }
 }
-
-
-
-//define main function
-var easycookie = (function () {
-    return {
-        add: function (name, val) { return eCI("add", name, val) },
-        edit: function (name, val) { return eCI("edit", name, val) },
-        del: function (name) { return eCI("del", name) },
-        get: function (name) { return eCI("get", name) },
-        ver: function () { eCI("ver") },
-        // popular aliases
-        set: function (name, val) { return eCI("add", name, val) },
-        change: function (name, val) { return eCI("edit", name, val) },
-        rem: function (name) { return eCI("del", name) },
-        remove: function (name) { return eCI("del", name) },
-        rm: function (name) { return eCI("del", name) },
-        check: function (name) { return eCI("get", name) },
-        version: function () { eCI("ver") },
-        about: function () { eCI("ver") },
-    };
-})();
--- beta/script.js
+++ beta/script.js
@@ -1,5 +1,51 @@
 // Variables
 const example = "<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu sapien vel felis luctus dictum iaculis a orci.</h3><h3> Proin accumsan, ligula vel ultricies varius, risus mi rhoncus sem, eget consequat velit enim vel nunc. In id augue vulputate, viverra ipsum non, facilisis lorem.</h3> <h3>Nam at neque sit amet erat scelerisque sagittis. Fusce semper faucibus nisi, sit amet viverra tellus vulputate a.</h3> <h3>Vivamus orci lorem, dignissim nec viverra vel, vulputate in leo. Vestibulum ut felis id quam cursus egestas. Integer aliquet scelerisque dictum.</h3> <h3>Integer et vulputate urna. Ut risus justo, mattis eu dapibus quis, vehicula non lorem. Duis eleifend est id lorem ultricies, quis consectetur quam posuere. Cras iaculis efficitur sollicitudin.</h3> <h3>Nam efficitur maximus efficitur. Sed dictum, sem in pulvinar auctor, nulla urna placerat lorem, ut egestas dolor enim quis dui.Aenean congue metus non varius consequat.</h3> <h3>Cras mollis est vitae neque iaculis interdum.Mauris dictum quam in mauris cursus, vel rutrum libero lobortis.Nam convallis nunc a diam mollis congue.</h3>";
+const privacyPolicy = 
+"<p>This Privacy Policy document contains types of information that is collected and recorded by Social Page and how we use it.</p>\
+<h2>Consent</h2>\
+<p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>\
+<h2>Information we collect</h2>\
+<p>Cookies</p>\
+<h2>How we use your information</h2>\
+<p>We use the information we collect in various ways, including to:</p>\
+<ul>\
+<li>Provide, operate, and maintain our website</li>\
+<li>Improve and expand our website</li>\
+<li>Understand and analyze how you use our website</li>\
+<li>Develop new products, services, features, and functionality</li>\
+</ul>\
+<h2>Log Files</h2>\
+<p>[EDIT]</p>\
+<h2>Cookies and Web Beacons</h2>\
+<p>Like any other website, Social Page uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>\
+<h2>Third Party Privacy Policies</h2>\
+<p>EasyCookieJS</p>\
+<p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</p>\
+<h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>\
+<p>Under the CCPA, among other rights, California consumers have the right to:</p>\
+<p>Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</p>\
+<p>Request that a business delete any personal data about the consumer that a business has collected.</p>\
+<p>Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</p>\
+<p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>\
+<h2>GDPR Data Protection Rights</h2>\
+<p>No sensitive data is collected</p>";
+
+const frameworksModal = 
+'<h3 style="font-weight: 500;">EasyCookieJS</h3>\
+<h3 style="font-weight: 500;">jQuery 3.6.0</h3>\
+<h3 style="font-weight: 500;">AnimateCSS</h3>\
+<h3 style="font-weight: 500;">Font Awesome 5.15</h3>\
+<h3 style="font-weight: 500;">Fonts Google: Comfortaa</h3>'
+
+const linksModal = 
+'<h3 style="font-weight: 500;">EasyCookieJS: <a href="https://github.com/Klubuntu/EasyCookieJS">Check</a></h3>\
+<h3 style="font-weight: 500;">jQuery 3.6.0: <a href="https://code.jquery.com/jquery-3.6.0.min.js">Check</a></h3>\
+<h3 style="font-weight: 500;">AnimateCSS: <a href="https://animate.style/">Check</a></h3>\
+<h3 style="font-weight: 500;">Font Awesome 5.15: <a href="https://use.fontawesome.com/releases/v5.15.4/fontawesome-free-5.15.4-web.zip">Check</a></h3>\
+<h3 style="font-weight: 500;">Comfortaa: <a href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap">Check</a></h3>'
+
+
+
 
 // Functions
 
@@ -23,40 +69,50 @@ function openpt() {
     open("bit.ly/rklbpt1");
 }
 
+function opendc(){
+    open("discord.com/invite/Y8Pux4uuPF");
+}
 
 
-function genmodal(modal, arrowBack, headerTitle, BodyCode, borderBody) {
-    if (arrowBack == true) {
+
+function genmodal(arrowBack, headerTitle, modalCode) {
+    if (arrowBack) {
         $("#back-action").fadeIn(0);
+        $("#menu").fadeOut(0);
+        $("#other").fadeIn(0);
     } else {
         $("#back-action").fadeOut(0);
+        $("#menu").fadeIn(0);
+        $("#other").fadeOut(0);
     }
     $("#modal-header-title").text(headerTitle);
-    $("#other-info").html(BodyCode);
+    $("#other").html(modalCode);
     h = $(".modal").height();
     w = $(".modal").width();
     console.log("H: ", h, " W: ", w)
 }
 
 function Privacy() {
-    genmodal("default", true, "Privacy Policy", example, "");
+    genmodal(true, "Privacy Policy", privacyPolicy);
 }
 
 function Frameworks() {
-    genmodal("default", true, "Frameworks", example, "");
+    genmodal(true, "Frameworks", frameworksModal);
 }
 
 function Links() {
-    genmodal("default", true, "Links", example, "");
+    genmodal(true, "Links", linksModal);
+}
+
+function Menu(){
+    genmodal(false, "About Page");
 }
 
 
-
 const openModal = (modal) => {
-    modal = document.getElementById("demo-modal");
+    modal = document.getElementById("page-modal");
     document.body.style.overflow = "hidden";
     $(modal).fadeOut(0);
-    //$(modal).fadeIn(550);
     modal.setAttribute("open", "true");
     document.addEventListener("keydown", escClose);
     let overlay = document.createElement("div");
@@ -65,7 +121,7 @@ const openModal = (modal) => {
 };
 
 const closeModal = (modal) => {
-    modal = document.getElementById("demo-modal");
+    modal = document.getElementById("page-modal");
     document.body.style.overflow = "auto";
     modal.removeAttribute("open");
     document.removeEventListener("keydown", escClose);
@@ -81,7 +137,6 @@ let i = 0;
 
 function updateprogress() {
     if (i == 3) {
-        //$("#start-loading").fadeOut();
         $("#end-loaing").fadeIn();
     }
     setTimeout(function() { $("progressv").html("<f>.</f>.."); }, 400);
@@ -101,81 +156,37 @@ function reportBug() {
     open("github.com/Klubuntu/EasyCookieJS");
 }
 
-//infoPagestr = "<h2 class='container-title'>Options</h2><option>Dark Mode</option><switch id='switchTheme'><div class='checkboxbg' id='switchthemebg'><div class='switch' id='switchtheme'></div></div></switch><option>No Icons</option><switch id='switchIcons'><div class='checkboxbg' id='switchiconsbg'><div class='switch' id='switchicons'></div></div></switch>";
-infoPagestr = `<h2 class="container-title">Options</h2><option>Dark Mode</option><switch id="switchTheme"><div class="checkboxbg" id="switchthemebg"><div class="switch" id="switchtheme"></div></div></switch><option>No Icons</option><switch id="switchIcons"><div class="checkboxbg" id="switchiconsbg"><div class="switch" id="switchicons"></div></div></switch><option style="position: relative;top: 10px;">Report</option><switch id="reportBug"><div class="checkboxbg" id="switchiconsbg"><div class="switch" style="width: 4vw; cursor: pointer;" id="switchicons"><p style="position: relative;top: 5px;" onclick="reportBug()" >Bug</p></div></div></switch>`;
-infoPrivacystr = "<h2 class='container-title'>Privacy</h2><h4>This page using Cookies to work</br> all saves switcher function</h4><h4>User cookies no use to ads,</br> optimize page and fix bugs.</h4>";
-infoIconsstr = "<h2 class='container-title'>Icons</h2><h4>Soon</h4><h4>Font Awesome 5.12</h4><h2 class='container-title'>Scripts</h2><h4>EasyCookie</h4><h5><a href='https://github.com/Klubuntu/EasyCookieJS'>https://github.com/Klubuntu/EasyCookieJS</a></h5><h4 style='position:relative;top: -13px;'>Jquery 3.6.1</h4>";
-infoAboutstr = "<h2 class='container-title'>About</h2><h4>Soon</h4><h5>&copy; Klubuntu (2020-2021)</h5>";
 let eThemeswitch = 0;
 let eIconsswitch = 0;
-backCSS = "<link id='default-style' rel='stylesheet' href='style.css' />";
-darkCSS = "<link id='dark-style' rel='stylesheet' href='dark-style.css' />";
 
-function infoPage() {
-    $("container").html(infoPagestr);
-    $("#title-modal").text("Page");
-    loadPage()
-}
 
-function infoPrivacy() {
-    $("container").html(infoPrivacystr);
-    $("#title-modal").text("Privacy");
-}
-
-function infoIcons() {
-    $("container").html(infoIconsstr);
-    $("#title-modal").text("Addons");
-}
-
-function infoAbout() {
-
-    $("container").html(infoAboutstr);
-    $("#title-modal").text("About");
+function backIcons() {
+    refresh();
 }
 
 function about() {
     openModal();
-    $("#about-info").fadeIn(450);
-    infoPage()
-}
-
-function abouthide() {
-    $("#about-info").fadeOut(450);
-}
-
-
-
-function removeIcons() {
-    $("svg").remove();
-}
-
-function backIcons() {
-    refresh();
 }
 
 
 function darkmode() {
     $("head").append(darkCSS);
     $("#default-style").remove();
-    easycookie.edit("theme", "dark");
-    //refresh();
+    EasyCookie.edit("theme", "dark");
 }
 
 function lightmode() {
     $("head").append(backCSS);
     $("#dark-style").remove();
-    easycookie.edit("theme", "light");
-    //refresh();
+    EasyCookie.edit("theme", "light");
 }
 // First Run or Check Theme
 setTimeout(function() {
-    x = easycookie.get("theme");
+    x = EasyCookie.get("theme");
     if (x == "") {
-        //alert("First Welcome User on Page")
-        easycookie.add("theme", "light");
+        EasyCookie.add("theme", "light");
     } else {
-        //alert("Next Welcome User on Page")
-        theme = easycookie.get("theme");
+        theme = EasyCookie.get("theme");
         if (theme == 'light') {
             lightmode();
         } else if (theme == 'dark') {
@@ -187,36 +198,9 @@ setTimeout(function() {
 
 
 
-// Get the modal
-
-
-// setInterval(function() {
-//     // setInterval(function() {
-//     //     if ($("#span").is(":hover")) {
-//     //         //window.location.href = "http://" + window.location.host + "/old"
-//     //         //$('#link').get(0).click();
-//     //         console.log("t1: complete");
-//     //     }
-//     // }, 2000);
-//     // ("#span").hover(function() {
-//     //         $("#span").show();
-//     //     },
-//     //     function() {
-//     //         $("#span").hide();
-//     //     });
-
-// }, 200);
 setTimeout(function() {
     $(".span-elements").fadeIn();
     $(".footer").fadeIn(650);
-    setInterval(function() {
-        if ($("#span").is(":hover")) {
-            //window.location.href = "http://" + window.location.host + "/old"
-            //$('#link').get(0).click();
-            console.log("t1: complete");
-        }
-    }, 2000);
-
 }, 1350);
 
 setTimeout(function() {
@@ -224,78 +208,11 @@ setTimeout(function() {
     $("#logo").fadeIn(650);
 }, 950);
 
-// setInterval(function() {
-//     $("switch").click(function() {
-//         var sw = ".switch"
-//         if (eswitch == 0) {
-//             $(sw).removeClass("unswitched")
-//             $(sw).removeClass("unswitched2")
-//             $(sw).addClass("switched")
-//             setTimeout(function() {
-//                 $(sw).addClass("switched2");
-//             }, 560);
-//             eswitch = 1
-//         } else if (eswitch == 1) {
-//             $(sw).removeClass("switched")
-//             $(sw).removeClass("switched2")
-//             $(sw).addClass("unswitched")
-//             setTimeout(function() {
-//                 $(sw).addClass("unswitched2");
-//             }, 560);
-//             eswitch = 0
-//         }
-
-//     })
-// }, 100);
 function loadPage() {
     $(document).ready(function() {
         $("#model-inner").on('resize', function() {
             console.log("e");
         });
         $("#end-loading").fadeOut();
-        $("#switchTheme").click(function() {
-            var sw = "#switchtheme";
-            var swb = "#switchthemebg";
-            $(swb).toggleClass("checked")
-            if (eThemeswitch == 0) {
-                $(sw).addClass("switched2")
-                $(sw).attr("style", "background-color: white;");
-                darkmode();
-
-                eThemeswitch = 1
-            } else if (eThemeswitch == 1) {
-                $(sw).removeClass("switched2")
-                $(sw).addClass("unswitched2")
-                $(sw).attr("style", "background-color: rgb(163, 15, 15);")
-                lightmode();
-                setTimeout(function() {
-                    $(sw).removeClass("unswitched2")
-
-                }, 620);
-
-                eThemeswitch = 0
-            }
-        })
-        $("#switchIcons").click(function() {
-            var sw = "#switchicons";
-            var swb = "#switchiconsbg";
-            $(swb).toggleClass("checked")
-            if (eIconsswitch == 0) {
-                $(sw).addClass("switched2")
-                $(sw).attr("style", "background-color: white;");
-                removeIcons();
-                eIconsswitch = 1
-            } else if (eIconsswitch == 1) {
-                $(sw).removeClass("switched2")
-                $(sw).addClass("unswitched2")
-                $(sw).attr("style", "background-color: rgb(163, 15, 15);");
-                setTimeout(function() {
-                    $(sw).removeClass("unswitched2")
-                    backIcons()
-                }, 620);
-                //backIcons();
-                eIconsswitch = 0
-            }
-        })
     })
 }
